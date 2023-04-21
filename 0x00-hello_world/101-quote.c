40810c7de370bbb5d9ccc1ceef452715919d3ec0
RuthNyankum <rnyankum99@gmail.com>
2023-04-21 00:54:21 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
-*main - standard error in c programming
-*Return 1 (Success)
+*main - Program that print the standard error
+*Return: 1 (Success)
 */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
