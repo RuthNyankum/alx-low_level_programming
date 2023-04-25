bf5c56f1dc0e2f215f58ddb1f17f7990e98a022b
RuthNyankum <rnyankum99@gmail.com>
2023-04-25 16:17:05 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet in lowcase
+*Return: void
+*/
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'b'; ch = 'y'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
