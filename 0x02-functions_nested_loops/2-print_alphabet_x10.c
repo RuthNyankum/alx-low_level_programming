62b7a37bf9d9927b8d08d1c33501d4c260e0ff42
RuthNyankum <rnyankum99@gmail.com>
2023-04-25 18:19:53 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - a function that prints 10 times the alphabet
+* in lowercase
+*/
+
+void print_alphabet_x10(void)
+{
+char snow;
+int t = 0;
+
+while (t++ <= 9)
+{
+for (snow = 'a'; snow <= 'z'; snow++)
+_putchar(t);
+_putchar('\n');
+}
