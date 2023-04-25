42e7e306c80e22d072ede152da9428b6306bb2f7
RuthNyankum <rnyankum99@gmail.com>
2023-04-25 18:47:30 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _islower - checks for lowercase character
+* Return: 1 if char is lowcase, otherwise 0.
+*/
+
+int _islower(int snow)
+{
+if (snow >= 'a' && snow <= 'z')
+return (1);
+else
+return (0);
+}
