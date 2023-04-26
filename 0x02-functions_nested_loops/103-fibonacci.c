fe57257d7ecadec262f3967d485ba4badbf7a3ad
RuthNyankum <rnyankum99@gmail.com>
2023-04-26 09:29:00 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Fibonacci sequence
+* Desription: prints the sum of the even-valued terms
+* Return: 0
+*/
+
+int main(void)
+{
+int a = 1, b = 2, snow = 0;
+int c;
+while (b < 4000000)
+{
+if (b % 2 == 0)
+snow += b;
+c = b;
+b = a;
+a = c;
+}
+printf("%d\n", snow);
+return (0);
+}
