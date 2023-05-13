f0be5eedfd9145c95f747178710e8e34e8b5d23d
RuthNyankum <rnyankum99@gmail.com>
2023-05-13 09:33:31 +0000

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,68 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
-
-/**
-* check_num - check string
-* @str: array string
-* Return: 0
-*/
-
-int check_num(char *str)
-{
-unsigned int snow;
-
-snow = 0;
-while (snow < strlen(str))
-
-{
-if (!isdigit(str[snow))
-{
-return (0);
-}
-
-snow++;
-}
-return (1);
-}
 
 /**
 * main - a program that adds positive numbers.
-* @argc: arguments count
-* @argv: arguments
+* @argc: argument count
+* @argv: argument
 * Return: 0
 */
 
 int main(int argc, char *argv[])
-
 {
+int t, u, addition = 0;
 
-int snow;
-int str_to_int;
-int sum = 0;
-
-snow = 1;
-while (snow < argc)
+for (t = 1; t < argc; t++)
 {
-if (check_num(argv[snow))
-
+for (u = 0; argv[u][u] != '\0'; u++)
 {
-str_to_int = atoi(argv[snow]);
-sum += str_to_int;
-}
-
-/*Condition if one of the number contains symbols that are not digits*/
-else
+if (!isdigit(argv[t][u]))
 {
 printf("Error\n");
 return (1);
 }
-
-snow++;
 }
-
-printf("%d\n", sum);
-
+addition += atoi(argv[t]);
+}
+printf("%d\n", addition);
 return (0);
 }
