3379289f86cb3f7a08d4f3e4a34d6b92d5902c8e
RuthNyankum <rnyankum99@gmail.com>
2023-06-01 23:31:50 +0000

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
+extern printf
 section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 0
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
 
 section .text
-    extern printf
-
-global main
+	global main
 main:
-    sub rsp, 8
-    mov rdi, format
-    mov rsi, hello
-    xor eax, eax
-    call printf
-    add rsp, 8
-    xor eax, eax
-    ret
+	push rbp
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
+
+	pop rbp
+	mov rax,0
+	ret
+
